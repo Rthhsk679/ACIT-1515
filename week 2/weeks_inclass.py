6f05cce4cf478f3c64304b6ac725dbf53bd85315
Jimmy Zhang <jimmyzhang@Jimmys-MacBook-Pro.local>
2026-09-17 16:00:18 -0700

    didnt do anything

---
+++ week 2/weeks_inclass.py
@@ -0,0 +1,36 @@
+# Create a variable named x and store an integer (whole number) inside it
+x = 2
+
+# Create a variable named y and store a string (any characters between single or double quotes) inside it
+y = "hi"
+
+# Create a variable named z and store a float in it
+z = "1.1"
+
+# Change the value stored in the x variable to a new string
+x = "bye"
+
+# Change the value stored in the y variable to a new boolean
+y = True
+
+# Change the value store in the z variable to a different float
+z = "1.2"
+
+# Print the value the user entered from the previous section
+output = input("input: " )
+print("output: " + output)
+
+# Print the (current) value of the variable x
+print(x)
+
+# Print the *type* (not the value itself) of the value stored in the y variable
+print(type(y))
+
+# Create two variables, one containing the string CIT, and another containing the string 1515
+class_name = "CIT"
+class_number = "1515"
+
+# Using the two variables and a hard-coded letter, print the word ACIT1515 to the terminal
+print("A " + class_name + class_number)
+
+print(f"{class_name}{class_number}")
